8d91431304a0c224e5d5f0339aa06deb2f683ffa
forteller11 <charly.yan.miller@gmail.com>
2020-02-18 05:28:30 -0500

    begun writing new code with render textures

---
+++ Assets/RotatePlayer/RenderToTexture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderToTexture : MonoBehaviour
+{
+    private Camera _camera;
+    public RenderTexture RenderTexture;
+    void Awake()
+    {
+        _camera = GetComponent<Camera>();
+        _camera.targetTexture = RenderTexture;
+    }
+    
+}
