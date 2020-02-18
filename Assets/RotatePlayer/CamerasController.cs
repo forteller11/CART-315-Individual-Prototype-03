c902f5c0d58ce8a1059fb9a9166b6d6ac557303a
forteller11 <charly.yan.miller@gmail.com>
2020-02-18 04:09:34 -0500

    adding more camera changes

--- Assets/RotatePlayer/CamerasController.cs
+++ Assets/RotatePlayer/CamerasController.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class CamerasController : MonoBehaviour
 {
     public Camera LeftEye;
-
     public Camera RightEye;
     
+    [Header("Eye Widths")]
+    [Range(0,1)]
     public float SideDisplayWidth = 0.2f;
+    [Range(0,1)]
     public float CenterDisplayWidth = 0.5f;
 
+    [Header("Rotation Offset")]
+    [Range(0,180f)]
+    public float SideRotationOffset = 60f;
+    [Range(0,180f)]
+    public float CenterRotationOffset = 0f;
+    
+    [Header("POV")] 
+    [Range(0,180f)]
+    public float SideFov = 110f;
+    [Range(0,180f)]
+    public float CenterFov = 50f;
+    
+    [Header("Transition")]
+    [Range(0,1)]
+    public float AmountToLerpToCenter = 0.1f;
+    [Range(0,1)]
+    public float AmountToLerpToSides = 0.01f;
+    
+    //lerp
+    [Header("Velocitys")]
     public float VelForSide = 0.5f;
     public float VelForCenter = 0f;
+    
     
     private float _amountCentered = 0.5f; //0 == side, 1 == centered
     private Rigidbody _rigidbody;
@@ -31,17 +54,40 @@ public class CamerasController : MonoBehaviour
     
     void PositionCameras()
     {
+        //_amountCentered = 0f;
         float displaySize = Mathf.Lerp(SideDisplayWidth, CenterDisplayWidth, _amountCentered);
-        
-        float leftPosition = _amountCentered;
-        float rightPosition = displaySize - _amountCentered;
-        
+
+        float centerLeftPosition = 0.5f;
+        float centerRightPosition = centerLeftPosition;
+        float sideLeftPosition = 0f;
+        float sideRightPosition = 1f - displaySize;
+
+
+        float leftPosition = Mathf.Lerp(sideLeftPosition, centerLeftPosition, _amountCentered);
+        float rightPosition = Mathf.Lerp(sideRightPosition, centerRightPosition, _amountCentered);
+            
         LeftEye.rect = new Rect(leftPosition,0f, displaySize, 1f);
         RightEye.rect = new Rect(rightPosition,0f, displaySize, 1f);
+
+        float rotationOffset = Mathf.Lerp(SideRotationOffset, CenterRotationOffset, _amountCentered);
+        LeftEye.transform.localRotation = Quaternion.Euler(0f, -rotationOffset, 0f);
+        RightEye.transform.localRotation = Quaternion.Euler(0f, rotationOffset, 0f);
+        
+        //POV
+        float fov = Mathf.Lerp(SideFov, CenterFov, _amountCentered);
+        LeftEye.fieldOfView = fov;
+        RightEye.fieldOfView = fov;
     }
 
     private void GetAmountToCenter()
     {
-        _amountCentered = Mathf.Lerp(1f,0f,(_rigidbody.velocity.magnitude - VelForCenter)/VelForSide);
+        float targetAmountToCenter = Mathf.Lerp(1f,0f,(_rigidbody.velocity.magnitude - VelForCenter)/VelForSide);
+        if (targetAmountToCenter > _amountCentered) //if going towards center
+            _amountCentered = Mathf.Lerp(_amountCentered, targetAmountToCenter, AmountToLerpToCenter);
+        else
+            _amountCentered = Mathf.Lerp(_amountCentered, targetAmountToCenter, AmountToLerpToSides);
+            
+        
+
     }
 }
